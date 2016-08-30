a03ff893cf68735cb63138672f18ebb613169f61
corinamarie <corina.bernstein@gmail.com>
2016-08-29 23:44:42 -0500

    adds TODO's for myself to plan out some more things to build

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -3,6 +3,9 @@ foxyApp.controller('FoxyCtrl', ['$scope', '$location', function($scope, $locatio
 
     console.log("FoxyCtrl is up");
 
+    //modal options for gallery?
+    //modal options for pricing/packages?
+
     //change route function
     $scope.changeRoute = function(route){
         $location.path(route);
